0c34f1db1e9eef2301581ec2dd84c64cde681657
Adam Hirsch <hirsch@axios.com>
2021-12-09 15:54:33 -0600

    day 9 pt 1 solved

--- 2021/09/df.go
+++ 2021/09/df.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -10,7 +11,47 @@ import (
 
 type depthMap struct {
 	dm         [][]int
-	riskLevels []int
+	riskLevels int
+}
+
+func (dm *depthMap) GetNeighbors(x, y int) ([]int, error) {
+	results := []int{}
+
+	if y > len(dm.dm) || x > len(dm.dm[0]) || x < 0 || y < 0 {
+		return []int{}, errors.New("invalid coordinate specified")
+	}
+
+	if y > 0 {
+		results = append(results, dm.dm[y-1][x])
+	}
+	if x != len(dm.dm[0])-1 {
+		results = append(results, dm.dm[y][x+1])
+	}
+	if y != len(dm.dm)-1 {
+		results = append(results, dm.dm[y+1][x])
+	}
+	if x > 0 {
+		results = append(results, dm.dm[y][x-1])
+	}
+
+	return results, nil
+}
+
+func (dm *depthMap) GetSizes() (int, int) {
+	return len(dm.dm) - 1, len(dm.dm[0]) - 1
+}
+
+func (dm *depthMap) GetRiskLevel(x, y int) int {
+	neighbors, _ := dm.GetNeighbors(x, y)
+
+	center := dm.dm[y][x]
+
+	for _, v := range neighbors {
+		if center >= v {
+			return 0
+		}
+	}
+	return center + 1
 }
 
 func getMap(f string) depthMap {
@@ -36,5 +77,12 @@ func getMap(f string) depthMap {
 }
 
 func main() {
-	fmt.Print(getMap("depths.txt"))
+	dm := getMap("depths.txt")
+	for y := 0; y < len(dm.dm); y++ {
+		for x := 0; x < len(dm.dm[0]); x++ {
+			dm.riskLevels += dm.GetRiskLevel(x, y)
+		}
+	}
+	fmt.Printf("Risk Levels Total: %d\n", dm.riskLevels)
+
 }
