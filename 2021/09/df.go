61538e1b3786b898d6d60e0e9f097a5dffb279dd
Adam Hirsch <adam.e.hirsch@gmail.com>
2021-12-10 15:11:43 -0600

    dumb bug!

--- 2021/09/df.go
+++ 2021/09/df.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -106,12 +107,10 @@ func (dm *depthMap) FloodFill(x, y int, tb *int, s *[]Point) (int, error) {
 
 	// the basins are bounded by 9-values
 	if dm.dm[y][x] == 9 {
-		fmt.Printf("NINE: %d, %d: %d (tb: %d)\n", x, y, dm.dm[y][x], tb)
 		return 0, nil
 	} else {
-		*tb += 1
 		// the square we're on is in a basin
-		fmt.Printf("HIT: %d, %d: %d (tb: %d)\n", x, y, dm.dm[y][x], tb)
+		*tb += 1
 	}
 
 	if y > 0 {
@@ -124,14 +123,14 @@ func (dm *depthMap) FloodFill(x, y int, tb *int, s *[]Point) (int, error) {
 		dm.FloodFill(x, y+1, tb, s)
 	}
 	if x > 0 {
-		dm.FloodFill(x-1, y-1, tb, s)
+		dm.FloodFill(x-1, y, tb, s)
 	}
 
 	return *tb, nil
 }
 
 func main() {
-	dm := getMap("sampledepths.txt")
+	dm := getMap("depths.txt")
 	for y := 0; y < len(dm.dm); y++ {
 		for x := 0; x < len(dm.dm[0]); x++ {
 			dm.riskLevels += dm.GetRiskLevel(x, y)
@@ -140,21 +139,25 @@ func main() {
 	fmt.Printf("Risk Levels Total: %d\n", dm.riskLevels)
 
 	toScan := []Point{}
+	basins := []int{}
 
 	for y := 0; y < len(dm.dm); y++ {
 		for x := 0; x < len(dm.dm[0]); x++ {
 			if alreadyScanned(x, y, &toScan) == false {
-				fmt.Printf("STARTING NEW BASIN: %d, %d\n", x, y)
 				tb := 0
 				bv, _ := dm.FloodFill(x, y, &tb, &toScan)
 				if bv != 0 {
-					fmt.Printf("%d, %d: %d\n", x, y, bv)
-
+					basins = append(basins, bv)
 				}
+
 			}
 
 		}
 	}
-	fmt.Print(toScan)
+
+	sort.Ints(basins)
+	fmt.Println(basins)
+	biggestIndex := len(basins) - 1
+	fmt.Printf("Day 2 solution: %d\n", basins[biggestIndex]*basins[biggestIndex-1]*basins[biggestIndex-2])
 
 }
