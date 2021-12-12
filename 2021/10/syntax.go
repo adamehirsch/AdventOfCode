04617f194a3ab52681ea43bfa1b7951230da1eca
Adam Hirsch <adam.e.hirsch@gmail.com>
2021-12-12 06:51:57 -0800

    more days

---
+++ 2021/10/syntax.go
@@ -0,0 +1,89 @@
+package syntax
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+var opens = map[string]string{"[": "]", "(": ")", "{": "}", "<": ">"}
+var closes = map[string]string{"]": "]", ")": ")", "{": "{", ">": ">"}
+
+type Line []string
+
+func (l *Line) String() string {
+	f := ""
+	for _, v := range *l {
+		f += fmt.Sprintf("%s ", v)
+	}
+	return f
+}
+
+func getText(f string) [][]string {
+	allStrings := [][]string{}
+	content, err := os.ReadFile(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// trim trailing newline
+	line := strings.TrimSuffix(string(content), "\n")
+
+	for _, v := range strings.Split(line, "\n") {
+		var row []string
+
+		for _, char := range strings.Split(v, "") {
+			row = append(row, char)
+		}
+		allStrings = append(allStrings, row)
+	}
+	return allStrings
+
+}
+
+func findFirstProblem(t Line) (string, bool) {
+	counter := []string{}
+	fmt.Println(t)
+	for i := 0; i < len(t); i++ {
+
+		v := t[i]
+		// fmt.Printf("%d: %s\n", i, v)
+		nextClose, isOpen := opens[v]
+		if isOpen == true {
+			counter = append(counter, nextClose)
+			// fmt.Println("\t", counter)
+			continue
+		}
+
+		thisClose, isClose := closes[v]
+		if isClose == true {
+			// fmt.Printf("  found %s at %d: removing %s\n", thisClose, i, counter[len(counter)-1])
+			// if the closing character is the top on the stack
+			if thisClose == counter[len(counter)-1] {
+				counter = counter[:len(counter)-1]
+				continue
+			} else {
+				// fmt.Printf("  found %s at %d: WRONG %s\n", thisClose, i, counter[len(counter)-1])
+
+				return v, true
+			}
+		}
+	}
+	return "", false
+}
+
+func main() {
+	allChars := getText("sample.txt")
+	for _, row := range allChars {
+		char, foundProblem := findFirstProblem(row)
+		if foundProblem {
+			for j := 0; j < len(row); j++ {
+				fmt.Printf("%s ", row[j])
+			}
+			fmt.Print("\n")
+			fmt.Println(char)
+		}
+
+	}
+
+}
---
+++ 2021/11/octo.go
@@ -0,0 +1,7 @@
+package octo
+
+import "aoc/crabs"
+
+func main() {
+	allOctos := crabs.GetCrabs("sample.txt")
+}
