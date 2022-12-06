20890132513e113037959c754acabc080a9af71d
Adam Hirsch <adam.e.hirsch@gmail.com>
2022-12-06 13:00:43 -0600

    in-progress day 3 2022

--- cmd/2203.go
+++ cmd/2203.go
@@ -16,22 +16,55 @@ var day2203Cmd = &cobra.Command{
 	Run:                   day2203Func,
 }
 
+var alphabetMap map[string]int
+
 func init() {
 	rootCmd.AddCommand(day2203Cmd)
+	alphabetMap = make(map[string]int)
+	for i, letter := range "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" {
+		alphabetMap[string(letter)] = i + 1
+	}
 }
 
-func findCommonLetter(s string) string {
+func findSplitLetters(s string) string {
 	l := strings.Split(s, "")
 	firstHalf := l[:len(l)/2]
 	secondHalf := l[len(l)/2:]
-	fmt.Println(firstHalf)
-	fmt.Println(secondHalf)
-	fmt.Println("")
-	return firstHalf[0]
+
+	return ""
+}
+
+func findCommonLetter(s ...string) string {
+	seen := make(map[rune]int)
+
+	first := s[0]
+
+	// for every letter in "first"
+	for _, r := range first {
+		// loop over each of the remaining sent strings
+		for _, c := range s[1:] {
+			// for every letter in each string
+			if strings.Contains(c, string(r)) {
+				seen[r]++
+			}
+		}
+	}
+
+	for k, v := range seen {
+		// if any letter has been seen as many times as
+		if v >= len(s) {
+
+		}
+	}
+
+}
+
+func letterValue(s string) int {
+	return alphabetMap[s]
 }
 
 func day2203Func(cmd *cobra.Command, args []string) {
-	wholeInput, err := utils.Opener("data/2203-sample.txt", true)
+	wholeInput, err := utils.Opener("data/2203.txt", true)
 
 	if err != nil {
 		fmt.Println(err)
@@ -39,9 +72,11 @@ func day2203Func(cmd *cobra.Command, args []string) {
 	}
 
 	lines := strings.Split(wholeInput, "\n")
+	total := 0
 
 	for _, line := range lines {
-		findCommonLetter(line)
+		total += letterValue(findSplitLetters(line))
 	}
+	fmt.Printf("Total: %d\n", total)
 
 }
