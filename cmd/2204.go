8b03f6f4117d3c6bfcc34f6f48fd65cee6b781a7
Adam Hirsch <adam.e.hirsch@gmail.com>
2022-12-08 07:06:35 -0600

    2204 p2 working

--- cmd/2204.go
+++ cmd/2204.go
@@ -20,24 +20,43 @@ func init() {
 	rootCmd.AddCommand(day2204Cmd)
 }
 
-func isOverlap(e []string) bool {
+func isOverlap(elves []string, phase int) bool {
 
-	var a, b, c, d int
+	var e1, e2, f1, f2 int
 
-	fmt.Sscanf(e[0], "%d-%d", &a, &b)
-	fmt.Sscanf(e[1], "%d-%d", &c, &d)
+	fmt.Sscanf(elves[0], "%d-%d", &e1, &e2)
+	fmt.Sscanf(elves[1], "%d-%d", &f1, &f2)
 
-	if a <= c {
-		if b >= d {
+	switch phase {
+	case 1:
+		if e1 <= f1 && e2 >= f2 {
 			return true
 		}
-	}
-	if a >= c {
-		if b <= d {
+		if e1 >= f1 && e2 <= f2 {
 			return true
 		}
+		return false
+	case 2:
+		switch {
+		// if any of the edge points overlap, it's an overlap
+		case e1 == f1 || e1 == f2 || e2 == f1 || e2 == f2:
+			return true
+
+		case e1 < f1 && f1 < e2:
+			return true
+		case f1 < e2 && e2 < f2:
+			return true
+
+		case f1 < e1 && e1 < f2:
+			return true
+		case e1 < f2 && f2 < e2:
+			return true
+
+		}
+		return false
 	}
 	return false
+
 }
 
 func day2204Func(cmd *cobra.Command, args []string) {
@@ -49,11 +68,16 @@ func day2204Func(cmd *cobra.Command, args []string) {
 	lines := strings.Split(wholeInput, "\n")
 
 	overlaps := 0
+	o2 := 0
 	for _, l := range lines {
 		elves := strings.Split(l, ",")
-		if isOverlap(elves) {
+		if isOverlap(elves, 1) {
 			overlaps++
+		}
+		if isOverlap(elves, 1) || isOverlap(elves, 2) {
+			o2++
 		}
 	}
 	fmt.Println("Phase 1: ", overlaps)
+	fmt.Println("Phase 2: ", o2)
 }
